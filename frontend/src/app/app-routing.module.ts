1674abaca7be6649139ecef0fa6ffb8970d7c0fd
Risonaldo Maciel <58106018+risonaldo@users.noreply.github.com>
2022-03-18 14:42:48 -0300

    Add files via upload

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+
+const routes: Routes = [{
+  path:"",
+  component: HomeComponent
+},{
+  path:"product",
+  component: ProductCrudComponent
+},
+{
+  path: "product/create",
+  component: ProductCreateComponent
+},
+{
+  path: "product/update/:id",
+  component: ProductUpdateComponent
+},
+{
+  path: "product/delete/:id",
+  component: ProductDeleteComponent
+},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,39 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product ={
+    id : 0,
+    name: '',
+    price: 0
+  };
+
+  constructor(private productService: ProductService, private router: Router) { };
+
+  ngOnInit(): void {
+   
+  };
+
+  createProduct():void{
+
+    this.productService.create(this.product).subscribe(() =>{
+      this.productService.showMessage('Produto Criado...');
+      this.router.navigate(['/product']);
+    }) ;
+  }
+ 
+  cancel():void{
+    this.router.navigate(['/product']);
+  };
+
+
+};
